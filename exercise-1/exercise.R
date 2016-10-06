b63269f6ebfa7a33ca4cb9156777fc035c17e09c
Benjamin <Benjamin.z.jin@gmail.com>
2016-10-06 14:12:20 -0700

    changed values to equations

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -11,8 +11,8 @@ puppies <- 2
 # Create a variable `puppy.price`, which is how expensive you think a puppy is
 puppy.price <- 200 #USD
 # Create a variable `total.cost` that has the total cost of all of your puppies
-total.cost <- 400
+total.cost <- puppies * puppy.price
 # Create a boolean variable `too.expensive`, set to true if the cost is greater than $1,000
-too.expensive <- FALSE
+too.expensive <- total.cost > 1000
 # Create a variable `max.puppies`, which is the nuber of puppies you can afford for $1K.
-max.puppies <- 5
+max.puppies <- 1000 / puppy.price
